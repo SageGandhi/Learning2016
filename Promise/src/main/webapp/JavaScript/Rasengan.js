6b36b0e9d634705a57f32d967d6d1d78c74af9bd
SageGandhi <prajit.ju@gmail.com>
2016-04-14 22:56:30 +0530

    Most Simplest JavaScript Utility

---
+++ Promise/src/main/webapp/JavaScript/Rasengan.js
@@ -0,0 +1,80 @@
+;(function(window,document,undefined){
+	var CallBackDomReadyQueue=[];
+	function Rasengan(Selector){
+		console.log('Inside Rasengan Constructor.');
+		if(!(this instanceof Rasengan)){
+			console.log('If Rasengan Invoked As Function.');
+			return new Rasengan(Selector);
+		}
+		if(typeof Selector==='function'){
+			return AddFunctionToDomReady(Selector);
+		}
+		this.SelectorLength=0,this.SelectorNode=[];
+		if(Selector instanceof HTMLElement||Selector instanceof NodeList){
+			/**Same As Array.prototype.slice.call===[].slice.call,Instantiate An Array Extra*/
+			this.SelectorNode=Selector.length>1?Array.prototype.slice.call(Selector):[Selector];
+		}else if(typeof Selector==='string'){
+			 if(Selector[0]==='<'&&Selector[Selector.length-1]===">"){
+				 this.SelectorNode=[CreateDivNode(Selector)];
+			 }else{
+				 this.SelectorNode=[].slice.call(document.querySelectorAll(Selector));
+			 }
+		}
+		if(this.SelectorNode.length){
+			this.SelectorLength=this.SelectorNode.length;
+			for(var Indx=0;Indx<this.SelectorLength;Indx++){
+				this[Indx]=this.SelectorNode[Indx];
+			}
+		}
+	};
+	function CreateDivNode(HtmlString) {var div=document.createElement('div');div.innerHTML=HtmlString;return div.firstChild;}
+	function AddFunctionToDomReady(CallBackDomReady){
+		return document.readyState==='complete'?CallBackDomReady.call(document,'Summoning Demonic Statue Of The Outer Path:Gedo Mazo'):CallBackDomReadyQueue.push(CallBackDomReady);
+	}
+	document.addEventListener('DOMContentLoaded',function DomContentLoadedHandler(){
+		document.removeEventListener('DOMContentLoaded',DomContentLoadedHandler);
+		while(CallBackDomReadyQueue.length){
+			CallBackDomReadyQueue.shift().call(document,'Summoning The Original');
+		}
+	});
+	Rasengan.ExtendingFunctionPrototype=Rasengan.prototype;
+	Rasengan.ExtendingFunctionPrototype.forEach=function(CallBack){
+		for(var Indx=0;Indx<this.length;Indx++){
+	        CallBack.call(this[Indx],this,Indx);
+	    }
+	    return this;
+	};
+	Rasengan.ExtendingFunctionPrototype.addClass=function(ClassNames){
+		return this.forEach(function(){
+			this.className+=' '+ClassNames;
+		});
+		return this;
+	};
+	Rasengan.ExtendingFunctionPrototype.removeClass=function(ClassNames){
+        return this.each(function() {
+            this.className=this.className.replace(new RegExp('\\b'+ClassNames+'\\b','g'),'');
+        });
+        return this;
+    };
+	Rasengan.ExtendingFunctionPrototype.setText=function(SetTextString) {
+	    if(SetTextString){
+	        return this.forEach(function(){
+	            this.innerText=SetTextString;
+	        });
+	    }
+	    return this;
+	};
+	Rasengan.ExtendingFunctionPrototype.on=function(EventType,CallBackHandler){
+	    return this.forEach(function(){
+	        this.addEventListener(EventType,CallBackHandler,false);
+	    });
+	    return this;
+	};
+	Rasengan.ExtendingFunctionPrototype.off=function(EventType,CallBackHandler){
+	    return this.forEach(function(){
+	        this.removeEventListener(EventType,CallBackHandler);
+	    });
+	    return this;
+	};
+	window.Rasengan=Rasengan;
+})(window,document);
