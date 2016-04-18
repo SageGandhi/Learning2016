570827943dc45d33c25e782f66b3d7cadff579f0
SageGandhi <prajit.ju@gmail.com>
2016-04-18 09:04:09 +0530

    Updated

---
+++ April.Learning/src/main/webapp/WEB-INF/Info.js
@@ -0,0 +1,14 @@
+/**Jquery First Named Jselect.Mainly Inspired By Prototype.js Which Is A Utility Focused To Javascript As A Language,For Browser Compatibility Jquery Built As A Side Project.
+ * But Domain Was Not Available,So Becomes Jquery.Simnon Wilson Wrote A JavaScript Library Called Css_Selector.js(GetElementBySelector(Using Css Selector)).Selector Pattern With
+ * Event,Plugin Architechture.Pie Is Also Knows As Position Is Everything.Regular Expression Has Its Own Syntax+Programming Language Syntax In Which That Regular Expressioin Is 
+ * Being Applied.Unix Utility Grep Means Globally Search For The Regular Expression & Print.[]Defines A Character Class,Only One Of The Enclosed Character In Character Class 
+ * Will Be Matched.Ex.[Aa] Will Match One Of 'A' Or 'a'.[.]Can Matches Any Character,Unless Programming Language Syntax Have Something To Do With It(New Line Character Does 
+ * Not Match In Some Languages).Quantifiers[?:0,1 Time Occurance Or Optional].Built In Character Classes:\s,\S,\d,\D,\w,\W.Other Quantifier *(0Or More/Kleene Star),
+ * +(One Or More),{n}(Exactly n Times),{n,}()At Least n Times),{n,m}(At Least n Times,At Most m Times).Quantifier Modifies Pattern To Its Immediate Left.()Defines Sub 
+ * Pattern.Regular Expression Engine Save/Capture The Portion Of A Input Text That Matches Parenthesized Group.Escaping Was Done By \(BackSlash).Greeiness:Quantifiers 
+ * Match As Much Text As Possibly Can,While Allowing Entire Pattern To Match Successfully.In Other Words Quantifier Greediness Will As Much Text As Possible,With ?
+ * Quantifier Along With Any Other Quantifier Is Lazyness(Opposite To Greedyness),Will Match As Few Text/Literal As Possible.^,$ Are Anchor For Text Search Start & End.
+ * The Statement Above That . Matches Any Character Is A Simplification For Pedagogical Purposes That Is Not Strictly True.Dot Matches Any Character Except Newline,"\n",
+ * But In Practice You Rarely Expect A Pattern Such As .+ To Cross A Newline Boundary.Perl Regexes Have A /s Switch & Java Pattern.DOTALL.Ex,To Make . Match Any Character 
+ * At All.For Languages That Don't Have Such A Feature,You Can Use Something Like [\s\S] To Match "Any Whitespace | Any Non-Whitespace",In Other Words Anything.*/
+/** Vim(VisualEditorImproved):TabStop=4,SoftTabStop=4,ShiftWidth=4,ExpandTab=4 */
---
+++ April.Learning/src/main/webapp/WEB-INF/Tools.js
@@ -0,0 +1,8 @@
+Utility={
+	"RegExp":[{"Href":"http://www.regexr.com/","Description":"Write Regular Expession & Test."},
+	          {"Href":"https://regexper.com","Description":"Regular Expession & Display Flow."},
+	          {"Href":"http://www.regexpal.com/","Description":"Regular Expession For Javascript Tester."},
+	          {"Href":"https://regex101.com/","Description":"Regular Expession Javascript With Debugger."},
+	          {"Href":"http://www.regular-expressions.info/|http://regexlib.com/","Description":"Regular Expession Info/Docs."}	          
+	         ]
+}
