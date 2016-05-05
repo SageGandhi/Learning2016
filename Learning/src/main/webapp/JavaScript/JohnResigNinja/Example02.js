2e0a223f914051d8939f0c033c3eea4c6588b17f
SageGandhi <prajit.ju@gmail.com>
2016-05-06 04:29:32 +0530

    Updated

--- Learning/src/main/webapp/JavaScript/JohnResigNinja/Example02.js
+++ Learning/src/main/webapp/JavaScript/JohnResigNinja/Example02.js
@@ -1,10 +1,8 @@
 /**
  * Web Related Discussion:http://thewebahead.net/| JeremyKeith:http://adactio.com/| CssDesign:http://www.csszengarden.com|The Document Object Model (DOM) Is The
  * Model That Describes How All Elements In An HTML Page, Like Input Fields, Images, Paragraphs Etc., Are Related To The Topmost Structure: The Document Itself. By
- * Calling The Element By Its Proper DOM Name, We Can Influence It.
+ * Calling The Element By Its Proper DOM Name, We Can Influence It.Jquery.1.4 Reference Guide|Learning Jquery|Jquery CookBook|Jeremy Keith(Dom Scripting)|Peter Paul
+ * Koch On JavaScript|Jonathon Snook-Advanced Dom Scripting,Ajax,Api,Library|Content(Html),Presentation(Css),Behavior(JavaScript)|Jquery:Find Something Then Do Something|
+ * Xcanvas With Mordeniser|Vector Markup Language|CanIUse.com For Feature Detection|JsFiddle|JsBin|Minifier:JsMin,DojoShrinkSafe,DeanEdwardsPacker,YuiCompressor,Google
+ * ClosureCompiler|Css Selectors Are One Based Index Like Array In JavaScript Is Zero Based Index| 
  */
-if (!Array.isArray) {
-	Array.isArray = function(Args) {
-		return Object.prototype.toString.call(Args) === '[object Array]';
-	}
-}
