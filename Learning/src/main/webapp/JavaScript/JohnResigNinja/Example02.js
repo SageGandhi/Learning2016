00cee5f93e53c98ce8fbfca64b0aaba30f49a991
SageGandhi <prajit.ju@gmail.com>
2016-05-06 08:21:56 +0530

    Updated

--- Learning/src/main/webapp/JavaScript/JohnResigNinja/Example02.js
+++ Learning/src/main/webapp/JavaScript/JohnResigNinja/Example02.js
@@ -4,5 +4,12 @@
  * Calling The Element By Its Proper DOM Name, We Can Influence It.Jquery.1.4 Reference Guide|Learning Jquery|Jquery CookBook|Jeremy Keith(Dom Scripting)|Peter Paul
  * Koch On JavaScript|Jonathon Snook-Advanced Dom Scripting,Ajax,Api,Library|Content(Html),Presentation(Css),Behavior(JavaScript)|Jquery:Find Something Then Do Something|
  * Xcanvas With Mordeniser|Vector Markup Language|CanIUse.com For Feature Detection|JsFiddle|JsBin|Minifier:JsMin,DojoShrinkSafe,DeanEdwardsPacker,YuiCompressor,Google
- * ClosureCompiler|Css Selectors Are One Based Index Like Array In JavaScript Is Zero Based Index| 
- */
+ * ClosureCompiler|Css Selectors Are One Based Index Like Array In JavaScript Is Zero Based Index|JavaScript First Introduced For Input Form Validation In Client Side To
+ * Reduce RoundTrip Time At The Age Of Telephone Modem(28.8Kbps Digital Subscriber Line).Minutes To Learn But Years To Master.Mocha,LiveScript(Client),LiveWire(Server),
+ * JavaScript.JavaScript In NetscapeNavigator,JScript In InternetExplorer.Standard Submitted To European Computer Manufacturers Association.Technical Committee #39(TC39)
+ * Was Assigned To “standardize the syntax and semantics of a general purpose, cross-platform, vendor-neutral scripting language”.Defined New Scripting Language Ecma-262,
+ * Pronounciated As(Ek-Ma).Adopted By International Organization For Standardization & International Electrotechnical Commission.JavaScript:=Core(EcmaScript)+Document 
+ * Object Model(Dom)+Browser Object Model(Bom).A Host Environment Provides The Base Implementation Of ECMAScript & Implementation Extensions Designed To Interface With The 
+ * Environment Itself.Host Environment Like Browser(ClientSide)/NodeJs(ServerSide)/AdobeFlash.JavaScripts Implements EcmaScript,As Well As Adobe ActionScript.Object Must Be
+ * Platform Independent.DOM Was A Need,To Manipulate Dhtml In CrossBrowser Manner.Document|Element|ProcessingInstruction|Comment|DocumentType|DocumentFragment|Text|
+ * CDATASection|EntityReference|Attr|Entity|Notation,Node|NodeList(Children Of A Node Ordered List)|NamedNodeMap(ElementsByTagName UnOrder Set Of Nodes)*/
